02f608434eee4c124b03b6b50a65b0d46379ec6f
BaileyV <baileyvarness@gmail.com>
2019-10-24 08:28:11 -0700

    first commit

---
+++ img/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    
+  /*$("#ninja-zero").click(function() {
+      $(this).attr("data-alt-src");
+    })*/
+
+  $("#ninja-zero").click(function(){
+    // Change src attribute of image
+    $(this).attr("src", "cat0.png");
+  }); 
+
+  $("#ninja-one").click(function(){
+    // Change src attribute of image
+    $(this).attr("src", "cat1.png");
+  }); 
+
+  $("#ninja-two").click(function(){
+    // Change src attribute of image
+    $(this).attr("src", "cat2.png");
+  }); 
+
+  $("#ninja-three").click(function(){
+    // Change src attribute of image
+    $(this).attr("src", "cat3.png");
+  });
+
+  $("#ninja-four").click(function(){
+    // Change src attribute of image
+    $(this).attr("src", "cat4.png");
+  });
+
+
+});
+
